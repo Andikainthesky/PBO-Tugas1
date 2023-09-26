0cd3373732dc6c1675b649e069474528b0ded1dc
Andikainthesky <andika.inthesky354@gmail.com>
2023-09-26 23:37:59 +0700

    Tipedata

---
+++ Java/Tipedata.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args){
+
+        String nama = "Andika Prayitno";
+        System.out.println("Nama Lengkap : " + nama);
+
+        int nama_awal = 'A';
+        System.out.println("Nama awalan : " + nama_awal);
+
+        float berat = 66.7f;
+        System.out.println("Berat Badan : " + berat);
+
+    }
+    
+}
