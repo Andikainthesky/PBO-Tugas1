27cfea4176b73bb5983031e509068a9d15111619
Andikainthesky <andika.inthesky354@gmail.com>
2023-09-26 15:57:41 +0700

    Main Program

---
+++ C++/latihan1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	cout << "Selamat siang"<<endl;
+	cout << "Andika Prayitno"<<endl;
+	cout << "Fakultas Teknologi Industri, Program Studi Informatika"<<endl;
+	cout << "2200018470";
+}
+
+
---
+++ Java/Main.java
@@ -0,0 +1,12 @@
+package belajar.Java;
+
+public class Main {
+    public static void main (String[] args) {
+        System.out.println("Selamat siang");
+
+        System.out.println("Andika Prayitno");
+        System.out.println("Fakultas Teknologi Industri, Program Studi Informatika");
+        System.out.println("2200018470");
+    }
+    
+}
